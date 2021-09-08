fcba7d77bd297ad024081e033e2b5e6bb4286e22
jailyne <arelissmatias@gmail.com>
2021-09-08 01:26:25 -0600

    pruebas en db

--- src/server.js
+++ src/server.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
-const fs = require('fs');
 const cors = require('cors');
-
+const db = require('./database.js');
 
 //Inicializacion    
 const app = express();
@@ -12,29 +11,56 @@ const app = express();
 
 
 //configuracion de servidor
-app.set('web','desarrolloUMG');
+app.set('web', 'desarrolloUMG');
 app.set('port', process.env.PORT || 5010);
 
 //Middlewares --> accion app
 app.use(cors());
 app.use(morgan('dev'));
-app.use(express.urlencoded({extended: false}));  
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 require('./database.js');
 
 
 
 
-app.get('/', function(rect,res){
+app.get('/', function (rect, res) {
     res.send('HOLAAAAA');
 });
 
-app.get("/list_movies", (req, res) => {
-    fs.readFile(__dirname + '/' + 'movies.json', 'utf8', (err, data) => {
-        res.end(data);
-    });
+app.get("/api/procedimientos", (req, res) => {
+    db.query('SELECT * FROM PROCEDIMIENTO', (error, rows) => {
+        if (error) {
+            throw error;
+        } else {
+            res.send(rows);
+        }
+    })
 });
 
+app.get("/api/procedimientos/:id", (req, res) => {
+    db.query('SELECT * FROM PROCEDIMIENTO WHERE id = ? ',[req.params.id] , (error, row) => {
+        if (error) {
+            throw error;
+        } else {
+            res.send(row);
+        }
+    })
+});
+
+app.post("/api/procedimientos/agregar", (req, res) => {
+    let data = {
+        nombre: req.body.nombre,
+        descripcion: req.body.descripcion,
+     }
+    db.query('INSERT INTO PROCEDIMIENTO SET ?',data, (error, result) => {
+        if (error) {
+            throw error;
+        } else {
+            res.send(result);
+        }
+    })
+});
 
 //Start
-app.listen(app.get('port'),()=> console.log('Server is running on port: ' + app.get('port') ));
+app.listen(app.get('port'), () => console.log('Server is running on port: ' + app.get('port')));
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,8 @@
 <template>
-  <h1>Hola soy el componente HOME</h1>
+  <v-container>
+    <h1>Hola soy el componente HOME</h1>
+    <v-btn v-on:click="prueba3()">agregar</v-btn>
+  </v-container>
 </template>
 
 <script>
@@ -9,16 +12,45 @@ export default {
   data() {
     return {
       parseResponse: [],
+      datos: {
+        nombre: "prueba",
+        descripcion: "descripcion prueba",
+      },
     };
   },
   created() {
     this.prueba();
+    this.prueba2();
   },
   methods: {
     async prueba() {
-      const response = await fetch("http://localhost:4000/list_movies");
+      const response = await fetch("http://localhost:5010/api/procedimientos");
       this.parseResponse = await response.json();
       return console.log(this.parseResponse);
+    },
+    async prueba2() {
+      const response = await fetch(
+        "http://localhost:5010/api/procedimientos/2"
+      );
+      this.parseResponse = await response.json();
+      return console.log(this.parseResponse);
+    },
+    async prueba3() {
+      const infor = {
+        payload: {
+          nombre: this.datos.nombre,
+          descripcion: this.datos.descripcion,
+        },
+      };
+      console.log(infor);
+      const response = await fetch(
+        "http://localhost:5010/api/procedimientos/agregar",
+        {
+          method: "POST",
+          body: JSON.stringify(infor),
+        }
+      );
+      return console.log(response.json());
     },
   },
 };
