4e64837445c0d8dd96e7cfb3fd8843835573bc22
jailyne <arelissmatias@gmail.com>
2021-09-08 18:44:08 -0600

    Se agrego un poco de estilo

--- src/components/CustomNavBar.vue
+++ src/components/CustomNavBar.vue
@@ -1,21 +1,40 @@
 <template>
-  <div>
-    <v-app-bar color="deep-purple accent-4" dense dark>
-      <v-app-bar-nav-icon></v-app-bar-nav-icon>
-      <v-toolbar-title>Aseguramiento de la calidad de software</v-toolbar-title>
+  <v-card class="overflow-hidden">
+    <v-app-bar
+      absolute
+      color="#0F3454"
+      dark
+      shrink-on-scroll
+      prominent
+      src="https://cutt.ly/LWJeeSp"
+      fade-img-on-scroll
+      scroll-target="#scrolling-techniques-4"
+    >
+      <template v-slot:img="{ props }">
+        <v-img
+          v-bind="props"
+          gradient="to top right, rgba(100,115,201,.7), rgba(25,32,72,.7)"
+        ></v-img>
+      </template>
+
+      <v-app-bar-nav-icon>
+        
+      </v-app-bar-nav-icon>
+
+      <v-toolbar-title>Diagramas de Flujo</v-toolbar-title>
+
       <v-spacer></v-spacer>
-      <v-btn icon>
-        <v-icon>mdi-heart</v-icon>
-      </v-btn>
+
       <v-btn icon>
         <v-icon>mdi-magnify</v-icon>
       </v-btn>
-    </v-app-bar>
-    <v-app-bar color="gray" max-height="45" dense >
+
+      <template v-slot:extension>
+  <v-app-bar color="gray" max-height="50"  dense >
       <v-row>
         <div v-for="item in items" v-bind:key="item.id" v-on:click="item.action">
           <v-col class="ma-0 pa-0">
-            <v-btn link :to="item.path" class="ma-0" elevation="0" small block >
+            <v-btn link :to="item.path" class="ma-0" elevation="5" small block >
               <span>
                 {{item.title}}
               </span>
@@ -24,8 +43,20 @@
         </div>
       </v-row>
     </v-app-bar>
-  </div>
+      </template>
+    </v-app-bar>
+    <v-sheet
+      id="scrolling-techniques-4"
+      class="overflow-y-auto"
+      max-height="225"
+    >
+      <v-container style="height: 1000px;"></v-container>
+    </v-sheet>
+  </v-card>
 </template>
+
+
+<!----------------------------------------------------- SCRIPT --------------------------------------------------------------------->
 
 <script>
 export default {
@@ -65,6 +96,4 @@ export default {
   }
 };
 </script>
-
-
 
--- src/views/CustomLogin.vue
+++ src/views/CustomLogin.vue
@@ -1,225 +1,96 @@
 <template>
-  <v-container data-app class="mt-16" >
-    <article>
-      <div class="container" :class="{ 'sign-up-active': signUp }">
-        <div class="overlay-container">
-          <div class="overlay">
-            <div class="overlay-left">
-              <h2>Welcome Back!</h2>
-              <p>Please login with your personal info</p>
-              <button class="invert" id="signIn" @click="signUp = !signUp">
-                Sign In
-              </button>
-            </div>
-            <div class="overlay-right">
-              <h2>Hello, Friend!</h2>
-              <p>Please enter your personal details</p>
-              <button class="invert" id="signUp" @click="signUp = !signUp">
-                Sign Up
-              </button>
-            </div>
+  <v-app>
+    <div class="backgruond"></div>
+    <v-main class="d-flex justify-center align-center">
+      <v-col cols="10" lg="4" class="mx-auto">
+        <v-card class="pa-4">
+          <div class="text-center">
+            <v-avatar size="100">
+              <v-img class="rounded-sm"
+                src="https://i.imgur.com/RJ5PGxt.png"
+                max-height="95"
+                max-width="95"
+              ></v-img>
+            </v-avatar>
+            <h2 class="indigo--text">Bienvenido Usuario</h2>
           </div>
-        </div>
-        <form class="sign-up" action="#">
-          <h2>Create login</h2>
-          <div>Use your email for registration</div>
-          <input type="text" placeholder="Name" />
-          <input type="email" placeholder="Email" />
-          <input type="password" placeholder="Password" />
-          <router-link :to="{ path: '/Home' }"
-            ><button>Regitrar</button></router-link
-          >
-        </form>
-        <form class="sign-in" action="#">
-          <h2>Sign In</h2>
-          <div>Use your account</div>
-          <input type="email" placeholder="Email" />
-          <input type="password" placeholder="Password" />
-          <a href="#">Forgot your password?</a>
-          <router-link :to="{ path: '/Home' }"
-            >Hola</router-link
-          >
-        </form>
-        <router-view></router-view>
-      </div>
-    </article>
-  </v-container>
+          <v-form @submit.prevent="submitHandler" ref="form">
+            <v-card-text>
+              <v-text-field
+                v-model="email"
+                :rules="emailRules"
+                type="email"
+                label="Email"
+                placeholder="Email"
+                prepend-inner-icon="mdi-account"
+                required
+              />
+              <v-text-field
+                v-model="password"
+                :rules="passwordRules"
+                :type="passwordShow ? 'text' : 'password'"
+                label="Password"
+                placeholder="Password"
+                prepend-inner-icon="mdi-key"
+                :append-icon="passwordShow ? 'mdi-eye' : 'mdi-eye-off'"
+                @click:append="passwordShow = !passwordShow"
+                required
+              />
+              <v-switch label="Remember me" color="indigo"></v-switch>
+            </v-card-text>
+            <v-card-actions class="justify-center">
+              <v-btn :loading="loading" type="submit" color="indigo">
+                <span class="white--text px-8">Login</span>
+              </v-btn>
+            </v-card-actions>
+          </v-form>
+        </v-card>
+      </v-col>
+    </v-main>
+    <v-snackbar top color="green" v-model="snackbar">
+      Login success
+    </v-snackbar>
+  </v-app>
 </template>
 
 <script>
 export default {
-  name: "CustomLogin",
-  data: () => {
-    return {
-      signUp: false,
-    };
+  name: "App",
+  data: () => ({
+    loading: false,
+    snackbar: false,
+    passwordShow: false,
+    email: "",
+    emailRules: [
+      (v) => !!v || "E-mail is required",
+      (v) => /.+@.+\..+/.test(v) || "E-mail must be valid",
+    ],
+    password: "",
+    passwordRules: [
+      (v) => !!v || "Password is required",
+      (v) => (v && v.length >= 6) || "Password must be 6  characters or more!",
+    ],
+  }),
+  methods: {
+    submitHandler() {
+      if (this.$refs.form.validate()) {
+        this.loading = true;
+        setTimeout(() => {
+          this.loading = false;
+          this.snackbar = true;
+        }, 3000);
+      }
+    },
   },
 };
 </script>
-
-<style lang="scss" scoped>
-.container {
-  position: relative;
-  width: 1900;
-  height: 480px;
-  border-radius: 10px;
-  overflow: hidden;
-  box-shadow: 0 15px 30px rgba(0, 0, 0, 0.2), 0 10px 10px rgba(0, 0, 0, 0.2);
-  background: linear-gradient(to bottom, #efefef, #ccc);
-  .overlay-container {
-    position: absolute;
-    top: 0;
-    left: 50%;
-    width: 50%;
-    height: 100%;
-    overflow: hidden;
-    transition: transform 0.5s ease-in-out;
-    z-index: 100;
-  }
-  .overlay {
-    position: relative;
-    left: -100%;
-    height: 100%;
-    width: 200%;
-    background: linear-gradient(to bottom right, #7fd625, #009345);
-    color: #fff;
-    transform: translateX(0);
-    transition: transform 0.5s ease-in-out;
-  }
-  @mixin overlays($property) {
-    position: absolute;
-    top: 0;
-    display: flex;
-    align-items: center;
-    justify-content: space-around;
-    flex-direction: column;
-    padding: 70px 40px;
-    width: calc(50% - 80px);
-    height: calc(100% - 140px);
-    text-align: center;
-    transform: translateX($property);
-    transition: transform 0.5s ease-in-out;
-  }
-  .overlay-left {
-    @include overlays(-20%);
-  }
-  .overlay-right {
-    @include overlays(0);
-    right: 0;
-  }
-}
-h2 {
-  margin: 0;
-}
-p {
-  margin: 20px 0 30px;
-}
-a {
-  color: #222;
-  text-decoration: none;
-  margin: 15px 0;
-  font-size: 1rem;
-}
-button {
-  border-radius: 20px;
-  border: 1px solid #009345;
-  background-color: #009345;
-  color: #fff;
-  font-size: 1rem;
-  font-weight: bold;
-  padding: 10px 40px;
-  letter-spacing: 1px;
-  text-transform: uppercase;
-  cursor: pointer;
-  transition: transform 0.1s ease-in;
-  &:active {
-    transform: scale(0.9);
-  }
-  &:focus {
-    outline: none;
-  }
-}
-button.invert {
-  background-color: transparent;
-  border-color: #fff;
-}
-form {
+<style>
+.backgruond {
+  height: 300px;
+  width: 100%;
+  display: block;
   position: absolute;
   top: 0;
-  display: flex;
-  align-items: center;
-  justify-content: space-around;
-  flex-direction: column;
-  padding: 90px 60px;
-  width: calc(50% - 120px);
-  height: calc(100% - 180px);
-  text-align: center;
-  background: linear-gradient(to bottom, #efefef, #ccc);
-  transition: all 0.5s ease-in-out;
-  div {
-    font-size: 1rem;
-  }
-  input {
-    background-color: #eee;
-    border: none;
-    padding: 8px 15px;
-    margin: 6px 0;
-    width: calc(100% - 30px);
-    border-radius: 15px;
-    border-bottom: 1px solid #ddd;
-    box-shadow: inset 0 1px 2px rgba(0, 0, 0, 0.4), 0 -1px 1px #fff,
-      0 1px 0 #fff;
-    overflow: hidden;
-    &:focus {
-      outline: none;
-      background-color: #fff;
-    }
-  }
-}
-.sign-in {
-  left: 0;
-  z-index: 2;
-}
-.sign-up {
-  left: 0;
-  z-index: 1;
-  opacity: 0;
-}
-.sign-up-active {
-  .sign-in {
-    transform: translateX(100%);
-  }
-  .sign-up {
-    transform: translateX(100%);
-    opacity: 1;
-    z-index: 5;
-    animation: show 0.5s;
-  }
-  .overlay-container {
-    transform: translateX(-100%);
-  }
-  .overlay {
-    transform: translateX(50%);
-  }
-  .overlay-left {
-    transform: translateX(0);
-  }
-  .overlay-right {
-    transform: translateX(20%);
-  }
-}
-@keyframes show {
-  0% {
-    opacity: 0;
-    z-index: 1;
-  }
-  49% {
-    opacity: 0;
-    z-index: 1;
-  }
-  50% {
-    opacity: 1;
-    z-index: 10;
-  }
+  background-size: cover;
 }
 </style>
